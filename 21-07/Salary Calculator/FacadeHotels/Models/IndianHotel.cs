944045937c72828366ba84662a7070201cc17d02
Prathmesh000 <98637538+Prathmesh000@users.noreply.github.com>
2024-08-10 12:43:52 +0530

    Add files via upload

---
+++ 21-07/Salary Calculator/FacadeHotels/Models/IndianHotel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacadeHotels.GlobalInterfaces;
+
+namespace FacadeHotels.Models
+{
+    internal class IndianHotel : IHotel
+    {
+        public IMenu GetMenu()
+        {
+            return new InidanMenu();
+        }
+    }
+}
---
+++ 21-07/Salary Calculator/FacadeHotels/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using FacadeHotels.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HotelReception hotelReception = new HotelReception();
+        hotelReception.GetItalianMenu();
+        hotelReception.GetIndianMenu();
+        hotelReception.GetChineseMenu();
+
+    }
+}
